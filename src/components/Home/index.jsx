80a873fa6f36e686c7bb160ca5fe924ab1c0cb79
Orlando Flores <orlando_flores@id.uff.br>
2021-11-01 16:09:16 -0300

    feat: adiciona SearchInput em Home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import DropDown from '../DropDown';
+import SearchInput from '../SearchInput';
 import productsCategories from '../../utils/categories';
 import sitesList from '../../utils/webSites';
 
 function Home() {
   const [category, setCategory] = useState("");
   const [webSite, setWebSite] = useState("");
+  const [query, setQuery] = useState("");
 
   return (
     <div >
       <DropDown categories={productsCategories} name="Categorias" value={category} setValue={setCategory} />
       <DropDown categories={sitesList} name="Sites" value={webSite} setValue={setWebSite} />
+      <SearchInput value={query} setValue={setQuery} />
     </div>
   );
 }
