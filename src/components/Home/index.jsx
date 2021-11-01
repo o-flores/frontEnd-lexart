63fb5d34a5ad4565101edd8ef0d85e50683d3ac8
Orlando Flores <orlando_flores@id.uff.br>
2021-11-01 15:58:05 -0300

    feat: cria componente Home e controla o estado dos DropDowns

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import DropDown from '../DropDown';
+import productsCategories from '../../utils/categories';
+import sitesList from '../../utils/webSites';
+
+function Home() {
+  const [category, setCategory] = useState("");
+  const [webSite, setWebSite] = useState("");
+
+  return (
+    <div >
+      <DropDown categories={productsCategories} name="Categorias" value={category} setValue={setCategory} />
+      <DropDown categories={sitesList} name="Sites" value={webSite} setValue={setWebSite} />
+    </div>
+  );
+}
+
+export default Home;
