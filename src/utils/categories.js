d4da384c9012c185a6bad77a1cc53d53fd2f710d
Orlando Flores <orlando_flores@id.uff.br>
2021-11-02 22:44:55 -0300

    fix: arquivo com as categorias. Adiciona id

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -1,1 +1,14 @@
-module.exports = ["Celular", "Geladeira", "Tv"];
+module.exports = [
+  {
+    name: "Celulares",
+    id: "MLB1055",
+  },
+  {
+    name: "Geladeiras",
+    id: "MLB181294",
+  },
+  {
+    name: "Tv",
+    id: "MLB1002",
+  }
+];
